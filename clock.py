f18c5f1195feeb25261696fdd27b94651074ba43
Magva7 <magva7@gmail.com>
2020-10-14 02:56:32 +0300

    Поправил - количество часов надо на 24 делить, а не на 23, ну и комменты на отдельные строки перенес

--- clock.py
+++ clock.py
@@ -1,9 +1,4 @@
 minut_count = int(input())
-
-# сколько часов прошло - делим кол-во минут на 60 считаем остаток
 result_hours = (minut_count // 60) % 23
-
-# сколько минут прошло - остаток от деления
 result_minut = minut_count % 60
-
 print(result_hours, result_minut)
