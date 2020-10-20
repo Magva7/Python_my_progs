3cd51b7525170e16c574275a2fa745acbfb0bcd0
Magva <magva7@gmail.com>
2020-10-20 06:15:38 +0300

    Пока не считает, т.к. например при 0,17 при умножении на 10 получается мало

--- test/test.py
+++ test/test.py
@@ -1,2 +1,4 @@
-a = 0.0
-print((a * a) * 10 // 1)
+parts_day = 0.17
+parts_day_calc = ((parts_day * parts_day) * 10 // 1)
+print('Нецелых дней для расчета: ', parts_day_calc)
+# print((parts_day * parts_day) * 10 // 1)
