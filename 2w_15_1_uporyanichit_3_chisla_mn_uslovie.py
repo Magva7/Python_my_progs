4bcb7bd89cb8af9922948c2967e3ef98c6cdc845
Magva7 <magva7@gmail.com>
2020-11-11 00:46:08 +0300

    Упорядочил 3 числа через плюсы и минусы

---
+++ 2w_15_1_uporyanichit_3_chisla_mn_uslovie.py
@@ -0,0 +1,16 @@
+a = 1
+b = 3
+if a > b:
+    (a, b) = (b, a)
+print(a,b)
+
+
+# c = 4
+# print(a, b, c)
+#
+# if a < b < c:
+#     print(a, b, c)
+# if a < b > c:
+#     print(a, c, b)
+# if a > b > c:
+#     print(c, b, a)
