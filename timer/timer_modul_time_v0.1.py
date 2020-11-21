a9eb6b3a56728563040ca1172e9f3b7ef86357c6
Magva7 <magva7@gmail.com>
2020-11-21 14:28:57 +0300

    Запилил готовый вариант - выводит время

--- timer/timer_modul_time_v0.1.py
+++ timer/timer_modul_time_v0.1.py
@@ -6,5 +6,8 @@ set_sec = set_min * 60
 
 while set_min != 0:
     set_sec -= 1  # каждую итерацию уменьшаем секунды на 1
-    print(time.strftime("Осталось: %M:%S", time.localtime(set_sec)))
+    print(time.strftime("Осталось: %M:%S", time.localtime(set_sec)), end='')
+    # выводим оставшее время, в конце end='', чтобы не выводило каждый раз с
+    # новой строки
     time.sleep(1)  # задержка - раз в секунду
+    print('\r', end='')
