e5a90a769728fe6b6ee8b4bcb033d3ada18e67eb
Magva7 <magva7@gmail.com>
2020-12-01 06:21:25 +0300

    Запилил несколько задач из мобильного учебника

---
+++ pythono_for_mobile/and.py
@@ -0,0 +1,6 @@
+myname = input('Введите логин: ')
+mypass = input('Введите пароль: ')
+if myname == 'magva' and mypass == 'superpass123':
+    print('Здорова, братан')
+else:
+    print('Ты кто такой, давай досвиданья...')
---
+++ pythono_for_mobile/or.py
@@ -0,0 +1,7 @@
+myname = input('Введите логин: ')
+mypass = input('Введите пароль: ')
+if myname == 'magva' and mypass == 'superpass123' or myname == 'marina' and \
+        mypass == 'masha2020':
+    print('Здорова, вижу свои')
+else:
+    print('Ты кто такой, давай досвиданья...')
