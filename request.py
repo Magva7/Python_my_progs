169f48a1a65aafc2707578f7ec0726f91f00d017
Magva7 <magva7@gmail.com>
2021-05-02 02:04:30 +0300

    Переписал под запрос с данными на следующие дни

--- request.py
+++ request.py
@@ -1,12 +1,28 @@
 import requests
 
 # appid = "2745926c9f2ffb7903aec82510e1bc65"  # мой id
-result = requests.get('https://api.openweathermap.org/data/2.5/weather?id=524901&appid=2745926c9f2ffb7903aec82510e1bc65&lang=RU')
-# отправляем запрос и записываем ответ от сервера в переменную
-# print(result.text)  # тестовый вывод - смотрим, что нам вернул сайт
-data = result.json()  # Разбираем пакет JSON, который получили - получаем
-# нужные значения по названиям полей
-vidimost = data['weather'][0]['description']
-print("видимость:", vidimost)
 
-# print(result.text)
+# отправляем запрос и записываем ответ от сервера в переменную, там данные по погоде на сейчас
+res = requests.get('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2745926c9f2ffb7903aec82510e1bc65&units=metric&lang=RU')
+
+data = res.json()  # Записываем в переменую пакет JSON, который получили - в ней есть поля и их значения
+
+# отправляем запрос и записываем ответ от сервера в переменную, там данные по погоде на следующие дни
+# zavtra_res = requests.get('https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=2745926c9f2ffb7903aec82510e1bc65&units=metric&lang=RU')
+# zavtra_data = today_res.json()  # Записываем в переменую пакет JSON, который получили - в ней есть поля и их значения
+
+# print(result.text)  # тестовый вывод - смотрим, что нам вернул сайт
+
+
+# получаем нужные значения по названиям полей
+print('Сейчас:')
+print("Видимость:", data['list'][0]['weather'][0]['description'])  # выбираем в массиве первый элемент - 0 и из него вытаскиваем значение полей, которые надо
+print('Ветрина:', data['list'][0]['wind']['speed'], 'м/с')
+print("За бортом:", data['list'][0]['main']['temp'], 'по цельсию')
+
+# print('Завтра:')
+# print(zavtra_data['weather'][0]['description'])
+# print( i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
+
+# for i in zavtra_data['list']:
+#     print( i['dt_txt'], '{0:+3.0f}'.format(i['main']['temp']), i['weather'][0]['description'])
