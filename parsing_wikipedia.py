f55bf6e2bf29a701f9ce68a112490bc09c0ed32d
Magva <magva7@gmail.com>
2021-05-25 18:02:19 +0300

    Написал парсер википедии

---
+++ parsing_wikipedia.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+
+resp = requests.get('https://wikipedia.org/')  # запрашиваем гл. стр. вики
+html = resp.text  # сохраняем страницу в переменной
+
+# print(html)  # test1
+
+soup = BeautifulSoup(html, 'lxml')  # распарсиваем страницу в переменную
+# print(soup)  # test2
+tags = soup('a', 'other-project-link')  # сохраняем в переменную содержимое тегов
+
+print([teg['href'] for teg in tags])  # перебираем содержимое, т.е. то, что в href
