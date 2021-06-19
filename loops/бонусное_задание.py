585e56dcb7a9414f997bb6f29f0d039d49adac5e
Magva7 <magva7@gmail.com>
2021-06-19 06:07:53 +0300

    Запилил я мега-ромб, но как, сам не понял, надо еще раз будет прямо с нуля сделать

--- loops/бонусное_задание.py
+++ loops/бонусное_задание.py
@@ -1,9 +1,9 @@
-strok_vsego = 5
+strok_vsego = 9
 current_strok_number = 1  # счетчик строк
 current_cifra = 1    # счетчик цифр в строке
 text = ''
 
-# добавляем строки до тех пор, пока номер текущей строки меньше, чем сколько всего строк
+# первая часть добавляем строки до тех пор, пока номер текущей строки меньше, чем сколько всего строк
 while current_strok_number <= strok_vsego:
 
     # строка набитая циклом выглядит так
@@ -21,48 +21,33 @@ while current_strok_number <= strok_vsego:
     text += '\n'  # добавляем в конце перенос строки
 
 current_strok_number += 1  # увеличиваем номер текущей строки
+# print(current_strok_number)
 
-print(text)
+# задаем новые значения переменным, теперь у нас номер строки начинается с n
+current_strok_number = strok_vsego  # счетчик строк
+current_cifra = 0    # счетчик цифр в строке
 
-# очищаем все переменные
-strok_vsego = 5
-current_strok_number = 5  # счетчик строк
-current_cifra = 1    # счетчик цифр в строке
-text = ''
-
-# пирамида в обратном порядке, чтобы получился ромб, добавляем строки до тех пор, пока номер текущей строки не будет = 1
-while current_strok_number != 1:
+# вторая часть, пока номер текущей строки не будет = 1
+while current_strok_number != 0:
 
     # строка набитая циклом выглядит так
-    current_strok_number -= 1  # сейчас равен 1
+    current_strok_number -= 1  # сейчас равен 5
 
-    # добавляем в начале строки пробелов столько, сколько всего строк, минус номер текущей строки, т.е. в первой
-    # строке в начале будет 4 пробела
-    text += str(' ' * (strok_vsego - current_strok_number))
-
-    # добавляем во все строки в конец пробелов столько, сколько всего строк
-    text += str('' * (strok_vsego))
-    current_cifra = current_strok_number
-
-    # потом добавляем числа с 1 до номера текущей строки, т.е. в первой строке будет 1, во 2 - 12
+    # добавляем во все строки в начало столько пробелов , сколько всего строк, минус текущая строка, минус 1
+    text += str(' ' * (strok_vsego - current_strok_number - 1))
+    # увеличиваем цифры на 1 и набиваем строку
+    current_cifra = 0
     while current_cifra <= current_strok_number:
-        text += str(current_cifra)
         current_cifra += 1
-
-    current_cifra = current_strok_number
-
-    # и в обратном порядке на уменьшение до 1
-    while current_cifra != 1:
-        current_cifra -= 1
         text += str(current_cifra)
 
-    # потом опять добавляем столько пробелов, сколько всего строк, минус номер текущей строки
-    text += str(' ' * (strok_vsego - current_strok_number))
+    # добавляем во все строки в конец пробелов столько, сколько всего строк, хотя это не обязательно
+    # text += str(' ' * (strok_vsego))
+
+
     text += '\n'  # добавляем в конце перенос строки
 
-current_strok_number += 1  # увеличиваем номер текущей строки
 
-text += '33'
 print(text)
 
 # строка 5 руками для теста
