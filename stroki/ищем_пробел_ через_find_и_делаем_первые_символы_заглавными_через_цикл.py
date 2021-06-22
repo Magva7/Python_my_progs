3f73a8e1a2d105e70a7843020aac7bfb1e7df21e
Magva7 <magva7@gmail.com>
2021-06-23 01:52:57 +0300

    Через find пока не делал - не факт, что сейчас надо

--- stroki/ищем_пробел_ через_find_и_делаем_первые_символы_заглавными_через_цикл.py
+++ stroki/ищем_пробел_ через_find_и_делаем_первые_символы_заглавными_через_цикл.py
@@ -1,10 +1,10 @@
 # обрабатываем строковые данные и возвращаем их с первыми заглавными буквами в каждом слове.
 
-# stroka = 'иванов иван иваныч иванько'
+stroka = 'иванов иван иваныч иванько'
 # stroka = 'ivan ivanov'
 # stroka = 'can    you   solve  it?'
 # stroka = 'abraсadabra'
-stroka = 'a1 2b  3   abc d3e r2D2'
+# stroka = 'a1 2b  3   abc d3e r2D2'
 
 stroka_list = list(stroka)  # записываем нашу строку в список, т.к. нам потом надо будет в ней менять символы,
 # а в питоне строку менять нельзя, это неизменяемый тип, зато можно перевести строку в список, поменять, что надо,
@@ -12,21 +12,25 @@ stroka_list = list(stroka)  # записываем нашу строку в сп
 # После обработки данных код должен выводить: "Иванов Иван"
 # логика следующая сначала проверяем, что у нас в первом символе именно буква, если буква, то всегда делаем заглавным
 # первый символ,
-# потом циклом пробегаемся по всем символам, ищем, на каком месте строки у нас пробел, потом делаем символ,
-# который идет за пробелом заглавным
+# потом циклом ищем, на каком месте строки у нас пробел,
+# сохраняем индекс этого символа в переменную, потом делаем символ, который идет за пробелом заглавным и начинаем
+# проверять дальше начиная с этого символа
 
 if stroka_list[0].isalpha() == True:  # проверяем, что первый символ это буква, если да, то
     stroka_list[0] = stroka_list[0].upper()  # делаем его большим
 
 # дальше у нас цикл - увеличиваем символы, после пробела
-# пробегаем во всем символам
-current_symbol_index = 1
-while current_symbol_index < len(stroka):
-    if (stroka_list[current_symbol_index] == ' '):  # если текущий символ пробел
-        if stroka_list[current_symbol_index + 1].isalpha() == True:  # тогда проверяем что следующий символ, это буква
-            stroka_list[current_symbol_index + 1] = stroka_list[current_symbol_index + 1].upper()  # делаем его большим
-    current_symbol_index += 1
+# ищем, какой индекс у первого пробела
+current_probel_index = stroka.find(' ')
+print(current_probel_index)  # печатаем индекс текущего пробела для теста
+
+# current_symbol_index = 1
+# while current_symbol_index < len(stroka):
+#     if (stroka_list[current_symbol_index] == ' '):  # если текущий символ пробел
+#         if stroka_list[current_symbol_index + 1].isalpha() == True:  # тогда проверяем что следующий символ, это буква
+#             stroka_list[current_symbol_index + 1] = stroka_list[current_symbol_index + 1].upper()  # делаем его большим
+#     current_symbol_index += 1
 
 stroka = ''.join(stroka_list)  # записываем получившийся список обратно в строку
 # print(stroka_list)  # результат в виде списка для теста
-print(stroka)  # результат в виде строки
+# print(stroka)  # результат в виде строки
